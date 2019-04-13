e1cee2eaef5998cbebb06675afc23f4e81bfecd7
redbeanzhuzhuanran <43868193+redbeanzhuzhuanran@users.noreply.github.com>
2019-04-14 03:21:48 +0800

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -11,17 +11,9 @@ int main()
 {
 	SelfTree::BinaryTree a;
 	a.CreatTree(cin, a.get_address());
-	
+	a.preTraverse(a.get_address()); //先序
+	a.midTraverse(a.get_address()); //中序
+	a.postTraverse(a.get_address()); //后序
     
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
